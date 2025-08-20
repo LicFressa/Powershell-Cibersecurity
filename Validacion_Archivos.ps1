4a4babe1ac97247846011b9b424ad79dedf2d832
Alberto Jessier Lucio Sital <alberto.lucios@uanl.edu.mx>
2025-08-20 00:22:30 -0600

    Add files via upload

---
+++ Validacion_Archivos.ps1
@@ -0,0 +1,21 @@
+﻿function Validar-Archivo {
+    param ([string]$Ruta)
+
+    try {
+        if (Test-Path $Ruta) {
+            $contenido = Get-content $Ruta -ErrorAction Stop
+            $peso = (Get-Item $Ruta | Measure-Object -Property Length -Sum).Sum
+            return "Archivo accesible: $Ruta `nPeso del Archivo: $peso bytes" 
+            
+        } else {
+            throw "El archivo no existe" 
+        }
+    }
+    catch {
+        return "Error: $_" 
+    }
+    Finally {
+        Write-Host "Evaluación terminada para: $Ruta `n" -ForegroundColor Cyan
+    }
+}
+
